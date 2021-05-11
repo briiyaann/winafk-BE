6fffcae95a74e1dd9ba6c795f4057ff25d678a1a
unknown <franzbrianbriones@gmail.com>
2021-05-11 08:32:00 +0800

    add softdeletes

--- app/Models/Core/SubmatchOdd.php
+++ app/Models/Core/SubmatchOdd.php
@@ -3,9 +3,12 @@
 namespace App\Models\Core;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SubmatchOdd extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['sub_match_id', 'match_id', 'team_id', 'bets', 'percentage', 'odds'];
 
     public function submatch()
